3998330fdc588472be7e174b44888eed8254eb3a
sunmengtuo <735490850@qq.com>
2019-12-11 14:59:15 +0800

    添加test_deque，test_list

--- 侯捷STL/my_test_stl.cpp
+++ 侯捷STL/my_test_stl.cpp
@@ -2,7 +2,7 @@
  * @Author: Cement
  * @Date: 2019-11-14 11:19:09
  * @LastEditors: Cement
- * @LastEditTime: 2019-12-06 23:42:22
+ * @LastEditTime: 2019-12-10 16:04:11
  * @Description: 
  */
 
@@ -84,15 +84,30 @@ int compareLongs(const void *v1, const void *v2)
     return (*(long *)v1 - *(long *)v2);
 }
 
-
 string get_target_string()
 {
     long target = 0;
-    char buf[10] ;
+    char buf[10];
     cout << "target(0~" << RAND_MAX << "):";
     cin >> target;
     snprintf(buf, 10, "%d", target);
     return string(buf);
+}
+
+int compareStrings(const void *v1, const void *v2)
+{
+    if (*(string *)v1 > *(string *)v2)
+    {
+        return 1;
+    }
+    if (*(string *)v1 < *(string *)v2)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 #include <iostream>
@@ -104,6 +119,8 @@ namespace tt01
 {
 void test_array()
 {
+
+    cout << "============test_array============" << endl;
 
     array<int, ASIZE> c;
     clock_t timeStart = clock();
@@ -132,42 +149,197 @@ void test_array()
 }
 } // namespace tt01
 
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdio> //snprintf()
 namespace tt02
 {
-void test_vector(long& elem_num)
+void test_vector(const long &elem_num)
 {
+    cout << "============test_vector============" << endl;
+
     vector<string> c;
     char buf[10];
-    for (long  i = 0; i < elem_num; i++)
-    {
-        snprintf(buf, 10, "%d", rand());
-        c.push_back(string(buf));
-    }
-    cout<<"vector.front()="<<c.front()<<endl;
-    cout<<"vector.back()="<<c.back()<<endl;
-    cout<<"vector.size()="<<c.size()<<endl;
-    cout<<"vector.capacity()="<<c.capacity()<<endl;
-    cout<<"vector.max_size()="<<c.max_size()<<endl;
-    cout<<"vector.data()="<<c.data()<<endl;
-    string target = get_target_string();
     clock_t timeStart = clock();
-    
-    auto pIte = ::find(c.begin(), c.end(), target);
-    
-    cout<<"::find:"<<clock()-timeStart<<" milli-seconds"<<endl;
-    if (pIte!=c.end())
+    for (long i = 0; i < elem_num; i++)
     {
-        cout<<"found:"<<*pIte<<endl;
-    }else
-    {
-        cout<<"not found"<<endl;
+        //有可能空间不够，push_back不进去，所以进行try
+        try
+        {
+            snprintf(buf, 10, "%d", rand());
+            //每个string是一个指针大小，这里是用char[]来初始化一个匿名的string对象
+            c.push_back(string(buf));
+        }
+        catch (const exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            abort(); //退出程序
+        }
     }
-    
+    cout << "milli-seconds=" << clock() - timeStart << endl;
+    cout << "vector.front()=" << c.front() << endl;
+    cout << "vector.back()=" << c.back() << endl;
+    cout << "vector.size()=" << c.size() << endl;
+    cout << "vector.capacity()=" << c.capacity() << endl;
+    cout << "vector.max_size()=" << c.max_size() << endl;
+    cout << "vector.data()=" << c.data() << endl;
+    string target = get_target_string();
+
+    auto pIte = ::find(c.begin(), c.end(), target);
+
+    cout << "::find:" << clock() - timeStart << " milli-seconds" << endl;
+    if (pIte != c.end())
+    {
+        cout << "found:" << *pIte << endl;
+    }
+    else
+    {
+        cout << "not found" << endl;
+    }
+
+    timeStart = clock();
+    ::sort(c.begin(), c.end());
+    string *pItem = (string *)bsearch(&target, c.data(), c.size(), sizeof(string), compareStrings);
+    cout << "qsort+bsearch:" << clock() - timeStart << " milli-seconds" << endl;
+    if (pItem != NULL)
+    {
+        cout << "found:" << *pItem << endl;
+    }
+    else
+    {
+        cout << "not found" << endl;
+    }
+    c.clear();
 }
 } // namespace tt02
+
+#include <iostream>
+#include <list>
+#include <cstdlib> //rand()
+#include <ctime>
+#include <cstdio> //snprintf()
+
+namespace tt03
+{
+void test_list(const long &num_elem)
+{
+    cout << "============test_list============" << endl;
+    list<string> c;
+
+    char buf[10];
+    clock_t timeStart = clock();
+    for (long i = 0; i < num_elem; i++)
+    {
+        try
+        {
+            snprintf(buf, 10, "%d", rand());
+            c.push_back(string(buf));
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            abort();
+        }
+    }
+    cout << "milli-seconds=" << clock() - timeStart << endl;
+    cout << "list.size()=" << c.size() << endl;
+    cout << "list.front()=" << c.front() << endl;
+    cout << "list.back()=" << c.back() << endl;
+    cout << "list.max_size()=" << c.max_size() << endl; //384307168202282325 这个属性代表什么
+
+    string target = get_target_string();
+
+    timeStart = clock();
+    list<string>::iterator pItem = ::find(c.begin(), c.end(), target);
+
+    cout << "::find()=" << clock() - timeStart << " milli-seconds" << endl;
+    if (pItem != c.end())
+    {
+        cout << "found:" << *pItem << endl;
+    }
+    else
+    {
+        cout << "not found" << endl;
+    }
+
+    timeStart = clock();
+
+    //自带sort()
+    c.sort();
+    cout << "list.sort()=" << clock() - timeStart << " milli-seconds" << endl;
+
+    c.clear();
+}
+} // namespace tt03
+
+#include <ext/slist> //非标准库的
+#include <iostream>
+#include <string>
+#include <cstdio>  //snprintf()
+#include <cstdlib> //rand()
+namespace tt04
+{
+void test_slist(const long &num_elem)
+{
+    cout << "============test_slist============" << endl;
+
+    //在__gnu_cxx命名空间下
+    __gnu_cxx::slist<string> c;
+}
+} // namespace tt04
+
+#include <iostream>
+#include <string>
+#include <deque>
+#include <ctime>
+#include <exception>
+namespace tt05
+{
+void test_deque(const long &num_elem)
+{
+    cout << "============test_deque============" << endl;
+
+    deque<string> c;
+
+    clock_t timeStart = clock();
+    char buf[10];
+    for (long i = 0; i < num_elem; i++)
+    {
+        try
+        {
+            snprintf(buf, 10, "%d", rand());
+            c.push_back(string(buf));
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            abort();
+        }
+    }
+    cout << "milli-seconds:" << clock() - timeStart << endl;
+    cout << "deque.size()" << c.size() << endl;
+    cout << "deque.front()" << c.front() << endl;
+    cout << "deque.back()" << c.back() << endl;
+    cout << "deque.max_size()" << c.max_size() << endl;
+
+    string target = get_target_string();
+    timeStart = clock();
+    auto pItem = ::find(c.begin(), c.end(), target);
+    cout << "::find(),milli seconds=" << clock() - timeStart << endl;
+    if (pItem != c.end())
+    {
+        cout << "found:" << *pItem << endl;
+    }
+    else
+    {
+        cout << "not found" << endl;
+    }
+    timeStart = clock();
+    ::sort(c.begin(), c.end());
+    cout << "::sort(),milli seconds=" << clock() - timeStart << endl;
+}
+} // namespace tt05
 
 int main()
 {
@@ -177,5 +349,8 @@ int main()
     //test001::test();
     //tt01::test_array();
     long a = 100000;
-    tt02::test_vector(a);
+    //tt02::test_vector(a);
+    //tt03::test_list(a);
+    //tt04::test_slist(a);
+    tt05::test_deque(a);
 }
